add74dc472bdbdd8c07a17d45059206ec0c8953e
기성환 <kish1919@gmail.com>
2023-05-25 15:38:12 -0400

    practice TodoList01-ch2-1

---
+++ todo7/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+          child: AppBar(), preferredSize: Size.fromHeight(0),),
+      body: Column(
+        children: [],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.today_outlined), label: "Today"),
+          BottomNavigationBarItem(icon: Icon(Icons.assignment_outlined), label: "Record"),
+          BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: "More")
+        ],
+        
+      ),// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
