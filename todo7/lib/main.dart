23cea0426a9f2f9cabcd780722d5105d0fdbca6d
기성환 <kish1919@gmail.com>
2023-05-26 14:25:32 -0400

    practice TodoList02-ch2-2

--- todo7/lib/main.dart
+++ todo7/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'data/todo.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -32,6 +34,25 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<Todo> todos = [
+    Todo(
+      title: "study1",
+      memo: "datasicence",
+      color: Colors.red.value,
+      done: 0,
+      category: "study",
+      date: 20230511
+    ),
+    Todo(
+        title: "study2",
+        memo: "datasicence",
+        color: Colors.blue.value,
+        done: 0,
+        category: "study",
+        date: 20230511
+    ),
+  ];
+
 
   void _incrementCounter() {
     setState(() {
@@ -44,9 +65,53 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: PreferredSize(
           child: AppBar(), preferredSize: Size.fromHeight(0),),
-      body: Column(
-        children: [],
+      body: ListView.builder(
+          itemBuilder: (ctx, idx){
+            if (idx ==0){
+              return Container(
+                child: Text("오늘하루", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+              );
+            } else if(idx==1){
+              return Container(
+                child: Column(
+                  children : List.generate(todos.length, (_idx){
+                    Todo t = todos[_idx];
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Color(t.color),
+                        borderRadius: BorderRadius.circular(16)
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(t.title, style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),),
+                              Text(t.done == 0 ? "미완료" :"완료", style: TextStyle(color: Colors.white),)
+                            ],
+                          ),
+                          Container(height: 8,),
+                          Text(t.memo, style: TextStyle(color: Colors.white))
+                        ],
+                      ),
+                    );
+                  // return Text("$_idx");
+                }),
+                ),
+              );
+            }
+
+            return Container();
+
+          },
+        itemCount: 4,
       ),
+
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
