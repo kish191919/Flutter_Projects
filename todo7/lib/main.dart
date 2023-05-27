ef6a3fbf2ff931e141c33b559ad89591d395b254
기성환 <kish1919@gmail.com>
2023-05-27 10:10:29 -0400

    practice TodoList03-ch2-3

--- todo7/lib/main.dart
+++ todo7/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo7/write.dart';
 
 import 'data/todo.dart';
+import 'data/utils.dart';
 
 void main() {
   runApp(MyApp());
@@ -47,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: "study2",
         memo: "datasicence",
         color: Colors.blue.value,
-        done: 0,
+        done: 1,
         category: "study",
         date: 20230511
     ),
@@ -65,6 +67,32 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: PreferredSize(
           child: AppBar(), preferredSize: Size.fromHeight(0),),
+      floatingActionButton : FloatingActionButton(
+        child: Icon(Icons.add, color: Colors.white,),
+        onPressed: ()async{
+
+          // 화면이동을 해야합니다.
+         Todo todo = await Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=>TodoWritePage(
+            todo: Todo(
+              title: "",
+              color: 0,
+              memo: "",
+              done: 0,
+              category: "",
+              date: Utils.getFormatTime(DateTime.now())
+            ),
+
+
+          )));
+
+         setState(() {
+           todos.add(todo);
+         });
+
+
+
+        },
+      ),
       body: ListView.builder(
           itemBuilder: (ctx, idx){
             if (idx ==0){
@@ -73,34 +101,54 @@ class _MyHomePageState extends State<MyHomePage> {
                 margin: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
               );
             } else if(idx==1){
+              List<Todo> undone = todos.where((t){
+                return t.done == 0;
+              }).toList();
               return Container(
                 child: Column(
-                  children : List.generate(todos.length, (_idx){
-                    Todo t = todos[_idx];
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: Color(t.color),
-                        borderRadius: BorderRadius.circular(16)
-                      ),
-                      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(t.title, style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),),
-                              Text(t.done == 0 ? "미완료" :"완료", style: TextStyle(color: Colors.white),)
-                            ],
-                          ),
-                          Container(height: 8,),
-                          Text(t.memo, style: TextStyle(color: Colors.white))
-                        ],
-                      ),
-                    );
+                  children : List.generate(undone.length, (_idx){
+                    Todo t = undone[_idx];
+                    return InkWell(child: TodoCardWidget(t: t),
+                    onTap: (){
+                      setState(() {
+                        if(t.done ==0){
+                          t.done = 1;
+                        }else{
+                          t.done = 0;
+                        }
+                      });
+                    },);
                   // return Text("$_idx");
                 }),
+                ),
+              );
+            } else if (idx ==2){
+              return Container(
+                child: Text("완료된 하루", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+              );
+            } else if(idx==3){
+              List<Todo> done = todos.where((t){
+                return t.done == 1;
+              }).toList();
+
+              return Container(
+                child: Column(
+                  children : List.generate(done.length, (_idx){
+                    Todo t = done[_idx];
+                    return InkWell(child: TodoCardWidget(t: t),
+                    onTap: (){
+                      setState(() {
+                        if(t.done==0){
+                          t.done = 1;
+                        }else{
+                          t.done = 0;
+                        }
+                      });
+                    },
+                    );
+                    // return Text("$_idx");
+                  }),
                 ),
               );
             }
@@ -110,13 +158,6 @@ class _MyHomePageState extends State<MyHomePage> {
           },
         itemCount: 4,
       ),
-
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.today_outlined), label: "Today"),
@@ -125,6 +166,40 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
         
       ),// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class TodoCardWidget extends StatelessWidget {
+
+  final Todo t;
+
+  TodoCardWidget({Key key, this.t}):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      decoration: BoxDecoration(
+          color: Color(t.color),
+          borderRadius: BorderRadius.circular(16)
+      ),
+      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(t.title, style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),),
+              Text(t.done == 0 ? "미완료" :"완료", style: TextStyle(color: Colors.white),)
+            ],
+          ),
+          Container(height: 8,),
+          Text(t.memo, style: TextStyle(color: Colors.white))
+        ],
+      ),
     );
   }
 }
---
+++ todo7/lib/write.dart
@@ -0,0 +1,142 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'data/todo.dart';
+
+class TodoWritePage extends StatefulWidget{
+
+  final Todo todo;
+
+  TodoWritePage({Key key, this.todo}): super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _TodoWritePageState();
+  }
+}
+
+class _TodoWritePageState extends State<TodoWritePage>{
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController memoController = TextEditingController();
+  int colorIndex = 0;
+  int ctIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          TextButton(
+              child: Text("저장", style: TextStyle(color: Colors.white),),
+          onPressed: (){
+                widget.todo.title = nameController.text;
+                widget.todo.memo = memoController.text;
+
+                Navigator.of(context).pop(widget.todo);
+
+                // 페이지 저장시
+          },)
+        ],
+      ),
+      body: ListView.builder(
+          itemBuilder: (ctx, idx){
+            if (idx == 0){
+              return Container(
+                child: Text("제목", style: TextStyle(fontSize: 20),),
+                margin: EdgeInsets.symmetric(vertical:12, horizontal: 16),
+              );
+            } else if (idx ==1){
+              return Container(
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  controller: nameController,
+                ),
+              );
+            } else if (idx ==2){
+              return InkWell(child: Container(
+                margin: EdgeInsets.symmetric(vertical:24, horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("색상",style: TextStyle(fontSize: 20),),
+                    Container(
+                      height: 20,
+                      width: 20,
+                      color: Color(widget.todo.color),
+                    )
+                  ],
+                ),
+              ),
+              onTap: (){
+                  List<Color> colors = [
+                    Color(0xFF80d3f4),
+                    Color(0xFFa794fa),
+                    Color(0xFFfb91d1),
+                    Color(0xFFfb8a94),
+                    Color(0xFFfebd9a),
+                    Color(0xFF51e29d),
+                    Color(0xFFFFFFFF),
+                  ];
+
+                  widget.todo.color = colors[colorIndex].value;
+                  colorIndex++;
+                  setState(() {
+
+                    colorIndex = colorIndex % colors.length;
+                  });
+
+              },
+              );
+            } else if (idx==3){
+              return InkWell(child: Container(
+                margin: EdgeInsets.symmetric(vertical:24, horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Category",style: TextStyle(fontSize: 20),),
+                    Text(widget.todo.category)
+                  ],
+                ),
+              ),
+              onTap: (){
+                List<String> category=["Study", "Meal", "WorkOut","Sleep"];
+
+                widget.todo.category = category[ctIndex];
+                ctIndex ++;
+                setState(() {
+                  ctIndex = ctIndex % category.length;
+                });
+
+
+              },
+              );
+            }else if (idx==4){
+              return Container(
+                margin: EdgeInsets.symmetric(vertical:12, horizontal: 16),
+                child: Text("Memo",style: TextStyle(fontSize: 20),),
+              );
+            }else if (idx==5){
+              return Container(
+                margin: EdgeInsets.symmetric(vertical:1, horizontal: 16),
+                child: TextField(
+                  controller: memoController,
+                  maxLines: 10,
+                  minLines: 10,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black))
+                  ),
+                ),
+              );
+            }
+            return Container();
+
+          },
+      itemCount: 6,),
+    );
+  }
+
+}
